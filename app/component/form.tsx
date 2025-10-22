2a9be885ee429a986f4839a10a53de6ec42ddba4
Nicolas-Eliazer-Jara <nicolas.jara.dev@gmail.com>
2025-10-22 13:04:58 -0300

    proyecto terminado

--- app/component/form.tsx
+++ app/component/form.tsx
@@ -10,64 +10,64 @@ export default function Form({ setProvincia, setElection }: FormProps) {
     <div className="px-4">
       <form>
         {/* En móviles se apilan, en pantallas grandes se alinean */}
-        <div className=" flex flex-col lg:flex-row lg:justify-around gap-4 md:gap-0">
+        <div className="flex flex-col lg:flex-row lg:justify-around gap-6">
           
           {/* Año */}
-          <div className="flex flex-col md:flex-row items-start md:items-center mb-3">
-            <h1 className="p-2 md:pr-5">Año:</h1>
-            <select className="bg-gray-200 rounded p-2 w-full md:w-[100%]  ml-[50px]">
-              <option value="2023">2023</option>
+          <div className="flex flex-col md:flex-row items-start md:items-center w-full lg:w-[30%]">
+            <h1 className="p-2 md:pr-5 whitespace-nowrap">Año:</h1>
+            <select className="bg-gray-200 outline-none rounded p-2 w-full  focus:border-blue-500 focus:ring-2 focus:ring-blue-300 ">
+              <option  value="2023">2023</option>
             </select>
           </div>
 
           {/* Resultados */}
-          <div className="flex flex-col  md:flex-row items-start md:items-center">
-            <h1 className="p-2 md:pr-5">Resultados:</h1>
+          <div className="flex flex-col md:flex-row items-start md:items-center w-full lg:w-[30%]">
+            <h1 className="p-2 md:pr-5 whitespace-nowrap">Resultados:</h1>
             <select
               onChange={(e) => setProvincia(e.target.value)}
-              className="bg-gray-200 rounded p-2 w-full md:w-[100%]"
+              className="bg-gray-200 rounded p-2 w-full focus:border-blue-500 focus:ring-2 focus:ring-blue-300"
             >
-              <option value="Argentina">Argentina</option>
-              <option value="Buenos Aires">Buenos Aires</option>
-              <option value="Ciudad Autónoma de Buenos Aires">
+              <option  value="Argentina">Argentina</option>
+              <option  value="Buenos Aires">Buenos Aires</option>
+              <option  value="Ciudad Autónoma de Buenos Aires">
                 Ciudad Autónoma de Buenos Aires
               </option>
-              <option value="Catamarca">Catamarca</option>
-              <option value="Chaco">Chaco</option>
-              <option value="Chubut">Chubut</option>
-              <option value="Córdoba">Córdoba</option>
-              <option value="Corrientes">Corrientes</option>
-              <option value="Entre Ríos">Entre Ríos</option>
-              <option value="Formosa">Formosa</option>
-              <option value="Jujuy">Jujuy</option>
-              <option value="La Pampa">La Pampa</option>
-              <option value="La Rioja">La Rioja</option>
-              <option value="Mendoza">Mendoza</option>
-              <option value="Misiones">Misiones</option>
-              <option value="Neuquén">Neuquén</option>
-              <option value="Río Negro">Río Negro</option>
-              <option value="Salta">Salta</option>
-              <option value="San Juan">San Juan</option>
-              <option value="San Luis">San Luis</option>
-              <option value="Santa Cruz">Santa Cruz</option>
-              <option value="Santa Fe">Santa Fe</option>
-              <option value="Santiago del Estero">Santiago del Estero</option>
-              <option value="Tierra del Fuego, Antártida e Islas del Atlántico Sur">
+              <option  value="Catamarca">Catamarca</option>
+              <option  value="Chaco">Chaco</option>
+              <option  value="Chubut">Chubut</option>
+              <option  value="Córdoba">Córdoba</option>
+              <option  value="Corrientes">Corrientes</option>
+              <option  value="Entre Ríos">Entre Ríos</option>
+              <option  value="Formosa">Formosa</option>
+              <option  value="Jujuy">Jujuy</option>
+              <option  value="La Pampa">La Pampa</option>
+              <option  value="La Rioja">La Rioja</option>
+              <option  value="Mendoza">Mendoza</option>
+              <option  value="Misiones">Misiones</option>
+              <option  value="Neuquén">Neuquén</option>
+              <option  value="Río Negro">Río Negro</option>
+              <option  value="Salta">Salta</option>
+              <option  value="San Juan">San Juan</option>
+              <option  value="San Luis">San Luis</option>
+              <option  value="Santa Cruz">Santa Cruz</option>
+              <option  value="Santa Fe">Santa Fe</option>
+              <option  value="Santiago del Estero">Santiago del Estero</option>
+              <option  value="Tierra del Fuego, Antártida e Islas del Atlántico Sur">
                 Tierra del Fuego, Antártida e Islas del Atlántico Sur
               </option>
-              <option value="Tucumán">Tucumán</option>
+              <option  value="Tucumán">Tucumán</option>
             </select>
           </div>
 
           {/* Tipo de elección */}
-          <div className="flex flex-col  md:flex-row items-start md:items-center">
-            <h1 className="p-2 md:pr-5">Tipo Elección:</h1>
+          <div className="flex flex-col md:flex-row items-start md:items-center w-full lg:w-[30%]">
+            <h1 className="p-2 md:pr-5 whitespace-nowrap">Tipo Elección:</h1>
             <select
               onChange={(e) => setElection(e.target.value)}
-              className="bg-gray-200 rounded p-2 w-full md:w-[100%]"
+              className="bg-gray-200 rounded p-2 w-full focus:border-blue-500 focus:ring-2 focus:ring-blue-300"
             >
-              <option value="Generales">Generales</option>
-              <option value="Balotage">Balotage</option>
+              <option  value="Generales">Generales</option>
+              <option  value="Balotage">Balotage</option>
             </select>
           </div>
         </div>
